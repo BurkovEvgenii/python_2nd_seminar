8868b7cac4865b5a0117407c973d60094f4c23c5
Burkov Evgenii <Burkovevgeniy@yansex.ru>
2022-10-14 23:12:06 +0300

    Finished 4th task

--- DZ_1_4.py
+++ DZ_1_4.py
@@ -2,3 +2,17 @@
 # Найдите произведение элементов на указанных позициях. 
 # Позиции вводятся с клавиатуры .
 
+from re import X
+
+
+N = int(input('write N number: '))
+dict = {}
+for i in range(-N , N+1):
+    dict[i] = i
+print(dict)
+X = int(input('write X position: '))
+Y = int(input('write Y position: '))
+mult = 0
+if X >= -N and Y <= N+1:
+    mult = dict[X] * dict[Y]
+    print(mult)
